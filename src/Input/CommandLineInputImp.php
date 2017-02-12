07a62f878c794caa9248a48bd83471366b09fc20
rolandodg <jesushs80@gmail.com>
2017-02-12 19:35:37 +0100

    Created Command Line Input class (implementation of Input interface).

---
+++ src/Input/CommandLineInputImp.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Phaba\Migrations\Input;
+
+
+use Phaba\Migrations\Core\Input\Input;
+
+class CommandLineInputImp implements Input
+{
+    private $args;
+
+    public function __construct(array $argv = null)
+    {
+        if($argv === null){
+            $argv = $_SERVER['argv'];
+        }
+
+        array_shift($argv);
+
+        $this->args = $argv;
+
+        if (!count($this->args)){
+            $this->setDefaults();
+        }
+    }
+
+    public function setDefaults()
+    {
+        $this->cliArguments[0] = 'help';
+    }
+}
