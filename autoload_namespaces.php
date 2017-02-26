f953d4ff4d7b6b5b2ac5aec1f74fc642477f17d1
rolandodg <jesushs80@gmail.com>
2017-02-26 11:37:51 +0100

    Removed Phaba- Migrations own autoload.

--- autoload_namespaces.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$baseDir = __DIR__ . '/src';
-
-return array(
-    'Phaba\\Migrations\\Application' => $baseDir.'/Application',
-    'Phaba\\Migrations\\Input' => $baseDir.'/Input',
-    'Phaba\\Migrations\\Help' => $baseDir.'/Help'
-);
