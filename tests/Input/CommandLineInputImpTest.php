17c36b47434816d7834b1da6a307c3febca84eb0
rolandodg <jesushs80@gmail.com>
2017-02-27 21:31:58 +0100

    Created unit tests for Command Line Input implementation (constructors, set default values).

---
+++ tests/Input/CommandLineInputImpTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phaba\Migrations\Tests\Input;
+
+
+use Phaba\Migrations\Input\CommandLineInputImp;
+use PHPUnit\Framework\TestCase;
+
+class CommandLineInputImpTest extends TestCase
+{
+    private function setGlobalServerArguments(string $scriptName = 'phaba-migration', string $command=null, array $arguments=[]): void
+    {
+        $_SERVER['argv'] = array($scriptName);
+        if  ($command !== null){
+            $_SERVER['argv'][] = $command;
+        }
+        foreach ($arguments as $argument){
+            $_SERVER['argv'][] = $argument;
+        }
+    }
+
+
+    public function testCanBeCreatedWithDefaultValues(): void
+    {
+        $this->setGlobalServerArguments();
+
+        $commandLineInput = new CommandLineInputImp();
+
+        $this->assertEquals('help',$commandLineInput->getFirstArgument());
+    }
+
+    public function testCanBeCreatedWithoutArguments(): void
+    {
+        $this->setGlobalServerArguments('phaba-migration','createTMNT', array('-m', '--michelangelo'));
+        $commandLineInput = new CommandLineInputImp();
+
+        $this->assertEquals('createTMNT',$commandLineInput->getFirstArgument());
+    }
+
+    public function testCanBeCreatedWithArguments(): void
+    {
+        $commandLineInput = new CommandLineInputImp(array('phaba-migration','createTMNT', '-m', '--michelangelo'));
+
+        $this->assertEquals('createTMNT', $commandLineInput->getFirstArgument());
+    }
+}
