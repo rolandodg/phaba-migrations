dbccaaee14143dbfcd79f69f76938f57a5602ee4
rolandodg <jesushs80@gmail.com>
2017-03-05 13:07:17 +0100

    Declared strict types on Command interface. Formatted code for following PSR-2 standard.

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -1,12 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 
 namespace Phaba\Migrations\Command;
 
-
 interface Command
 {
-    public function execute();
-    public function validateArguments();
-    public function tryToValidateArguments();
+    public function execute(): void;
+    public function validateArguments(): void;
 }
