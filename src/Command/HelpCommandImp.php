4495a3af2547d26503fc6d731b9836d87ec8e958
rolandodg <jesushs80@gmail.com>
2017-03-11 17:57:35 +0100

    Created Help Command implementation.

---
+++ src/Command/HelpCommandImp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Phaba\Migrations\Command;
+
+use Phaba\Migrations\Command\Command;
+use Phaba\Migrations\Input\CommandLineArguments;
+use InvalidArgumentException;
+use Phaba\Migrations\Output\Output;
+
+class HelpCommandImp implements Command
+{
+    private $argv;
+
+    public function __construct(CommandLineArguments $argv)
+    {
+        $this->argv = $argv;
+    }
+
+    public function execute(): Output
+    {
+        $this->validateArguments();
+    }
+
+    public function validateArguments(): void
+    {
+        if (count($this->argv) > 1) {
+            throw new InvalidArgumentException(sprintf('Invalid parameter %s', $this->argv[1]));
+        }
+    }
+}
