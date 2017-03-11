79afd1ffffc74b3c5c6bc22c1009dccfaa21e5ea
rolandodg <jesushs80@gmail.com>
2017-03-11 17:56:49 +0100

    Fixed bug with returned type from command execute function

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 
 namespace Phaba\Migrations\Command;
 
+use Phaba\Migrations\Output\Output;
+
 interface Command
 {
-    public function execute(): void;
+    public function execute(): Output;
     public function validateArguments(): void;
 }
