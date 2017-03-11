495c6872b97e55e223af044e6e59c1f9cf8cac06
rolandodg <jesushs80@gmail.com>
2017-03-11 17:58:26 +0100

    Created Migration File Generator Class

---
+++ src/File/MigrationFileGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phaba\Migrations\File;
+
+use Phaba\Migrations\Output\Output;
+
+class MigrationFileGenerator
+{
+    public function tryToCreate(): Output
+    {
+    }
+}
