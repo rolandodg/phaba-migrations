6046deba6e823037d43ec42a4074631366db2b9b
rolandodg <jesushs80@gmail.com>
2017-03-25 11:09:39 +0100

    Added Configuration Reader interface constructor which receive an string as parameter (configuration path).

--- src/Core/Configuration/ConfigurationReader.php
+++ src/Core/Configuration/ConfigurationReader.php
@@ -6,5 +6,6 @@ namespace Phaba\Migrations\Core\Configuration;
 
 interface ConfigurationReader
 {
+    public function __construct(string $configurationPath);
     public function getElement(string $name);
 }
