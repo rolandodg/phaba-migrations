0c701a0740f3cd1d5f5d89f0807229bde5773203
rolandodg <jesushs80@gmail.com>
2017-03-26 12:00:29 +0200

    Added process for creating empty migration file (with unit tests).

--- src/File/MigrationFileGenerator.php
+++ src/File/MigrationFileGenerator.php
@@ -4,11 +4,61 @@ declare(strict_types=1);
 
 namespace Phaba\Migrations\File;
 
+use Phaba\Migrations\Factory\Output\CommandLineOutputFactory;
 use Phaba\Migrations\Output\Output;
+use PHPUnit\Framework\Exception;
 
 class MigrationFileGenerator
 {
-    public function tryToCreate(): Output
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var Output
+     */
+    private $output;
+
+    public function __construct(string $path)
     {
+        $this->path = $path;
+
+        $outputFactory = new CommandLineOutputFactory();
+        $this->output = $outputFactory->createOutput();
+    }
+
+    public function create(\DateTime $now): Output
+    {
+        try {
+            $this->tryToCreate($now);
+        } catch (Exception $e) {
+            $this->output->setContent(array($e->getMessage()));
+        }
+
+        return $this->output;
+    }
+
+    private function tryToCreate(\DateTime $now): void
+    {
+        if (!file_exists($this->path)) {
+            mkdir($this->path);
+        }
+
+        if (!file_exists($this->path.'/'.$this->generateFileName($now).'php')) {
+            $file = fopen($this->path.'/'.$this->generateFileName($now).'php', 'wb');
+            fclose($file);
+            $this->output->setContent(array('Created file '.$this->generateFileName($now).".php in $this->path"));
+        } else {
+            $this->output->setContent(
+                array(
+                    'Cannot create migration file '.$this->generateFileName($now).".php. File already exists in $this->path")
+            );
+        }
+    }
+
+    private function generateFileName(\DateTime $now): string
+    {
+        return $now->format('YmdHis');
     }
 }
